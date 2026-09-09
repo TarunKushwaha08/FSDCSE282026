e235f3836ac2e0e21bfeb1a529ef6f4eff2b99ed
ptomer40 <ptomer40@yahoo.com>
2026-09-09 10:24:06 +0530

    React vite demo

---
+++ reactvite/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import ICard from './components/ICard'
+
+function App() {
+ 
+
+  return (
+    <div style={{border:'10px solid red', height:'300px', width:'300px'}}>
+  <h2>Welcome to React vite</h2>   
+    <ICard />
+    </div>
+  )
+}
+
+export default App
